a3c11c02efb08336a22c752231e174276906b2b4
j m <jm@js-MacBook-Pro.local>
2019-07-08 10:56:20 +0200

    setting files

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,19 @@
-let canvas = document.getElementById("canvas");
+let canvas = document.getElementById("canvas")
+let ctx =canvas.getContext('2d');
 
-let ctx = canvas.getContext('2d');
+let intervalId = setInterval(function(){
+  ctx.clearRect(0, 0, width, height);
+  drwaScore();
+  snake.move();
+  snake.draw();
+  apple.draw();
+  drawBorder();
+}, 100);
+
+let width = canvas.width;
+let height = canvas.height;
+let blockSize = 10;
+let widthInBlocks = width / blockSize;
+let heightInBlocks = height / blockSize;
+let score = 0;
+
