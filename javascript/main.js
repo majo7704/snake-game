80929f19b4d1585446b42c36da97e3652457d515
j m <jm@js-MacBook-Pro.local>
2019-07-10 14:23:38 +0200

    start button/pause works

--- javascript/main.js
+++ javascript/main.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function () {
 
-
+//initiating canvas
 let canvas = document.getElementById("canvas")
 let ctx =canvas.getContext('2d');
 
@@ -13,9 +13,10 @@ let blockSize = 10;
 let widthInBlocks = width / blockSize;
 let heightInBlocks = height / blockSize;
 let score = 0;
+let isGameOver = false;
 
 
-
+//Gray border arround canvas
 let drawBorder = function (){
   ctx.fillStyle = 'Gray'
   ctx.fillRect(0, 0, width, blockSize);
@@ -23,31 +24,33 @@ let drawBorder = function (){
   ctx.fillRect(0, 0, blockSize, height);
   ctx.fillRect(width - blockSize, 0, blockSize, height)
 };
-  let drawScore = function () {
-    ctx.font = " 20px Bahiani";
-    ctx.fillStyle = "Black";
-    ctx.textAlign = "center";
-    ctx.textBaseLine = "top";
-    ctx.fillText("Score: " + score, 50, 30)
-  };
-
-
-  let gameOver = function () {
-    clearInterval(intervalId);
-    ctx.font = "6vh Bahiani";
-    ctx.fillStyle = "Black";
-    ctx.textAlign = 'center'; 
-    ctx.textBaseLine = "middle";
-    let imgGameOver = new Image(10, 10);
-    imgGameOver.src = '../javascript/img/THE-END.png';
-    imgGameOver.onload = function(){
-    ctx.drawImage(imgGameOver, 70, 50);
-    }
-    let audio = new Audio('../javascript/audio/382310__myfox14__game-over-arcade.wav');
-    audio.play();
-  };
- 
-
+//Score with canvas
+let drawScore = function () {
+  ctx.font = " 20px Bahiani";
+  ctx.fillStyle = "Black";
+  ctx.textAlign = "center";
+  ctx.textBaseLine = "top";
+  ctx.fillText("Score: " + score, 50, 30)
+};
+//Function that clears intervalId in order to finish the game
+let gameOver = function () {
+  clearInterval(intervalId);
+  isGameOver = true;
+  
+  // ctx.font = "6vh Bahiani";
+  // ctx.fillStyle = "Black";
+  // ctx.textAlign = 'center'; 
+  // ctx.textBaseLine = "middle";
+  let imgGameOver = new Image(10, 10);
+  imgGameOver.src = '../javascript/img/THE-END.png';
+  imgGameOver.onload = function(){
+  ctx.drawImage(imgGameOver, 70, 50);
+  }
+  let audio = new Audio('../javascript/audio/382310__myfox14__game-over-arcade.wav');
+  audio.play();
+  //Uncaught (in promise) DOMException
+};
+ //Function that creates a circle which will be used to create an apple
 let circle = function(x, y, radius, fillCircle) {
   ctx.beginPath();
   ctx.arc(x, y, radius, 0, Math.PI * 2, false)
@@ -62,14 +65,13 @@ let Block = function (col, row) {
   this.col = col;
   this.row = row;
 }
-
+//Block methods
 Block.prototype.drawSquare = function(color) {
   let x = this.col * blockSize ;
   let y = this.row * blockSize ;
   ctx.fillStyle = color;
   ctx.fillRect(x, y, blockSize, blockSize);
 };
-
 Block.prototype.drawCircle = function(color) {
   let centerX = this.col * blockSize + blockSize / 2;
   let centerY = this.row * blockSize + blockSize / 2;
@@ -81,7 +83,7 @@ Block.prototype.drawCircle = function(color) {
 Block.prototype.equal = function (otherBlock) {
   return this.col === otherBlock.col && this.row === otherBlock.row;
 }
-
+//Snake constructor
 let Snake = function () {
   this.segments = [
     new Block(7, 5),
@@ -92,22 +94,12 @@ let Snake = function () {
   this.direction = "right";
   this.nextDirection = "right"
 }
+//Snake methods
 Snake.prototype.draw = function () {
   for (let i = 0; i < this.segments.length; i++){
     this.segments[i].drawSquare("Orange")
   }
 }
-let Snake2 = function () {
-  this.segments = [
-      new Block(10, 2),
-      new Block(11, 2),
-      new Block(12, 3),
-      new Block(13, 2)
-  ]
-  this.direction = "right";
-  this.nextDirection = "right"
-}
-
 Snake.prototype.move = function (){
   var head = this.segments[0];
   var newHead;
@@ -125,12 +117,13 @@ Snake.prototype.move = function (){
   } else if (this.direction === 'stop') {
     newHead = new Block(head.col, head.row);
   } 
-
+  
   if (this.checkCollision(newHead)) {
     gameOver();
     return;
   }
   this.segments.unshift(newHead);
+  
 
   if (newHead.equal(apple.position)) {
     let audio = new Audio('../javascript/audio/20280__koops__apple-crunch-17.wav');
@@ -159,6 +152,8 @@ Snake.prototype.checkCollision = function (head) {
   return wallCollision || selfCollision;
 };
 Snake.prototype.setDirection = function(newDirection) {
+  
+
   if (this.direction === "up" && newDirection === "down") {
     return;
   } else if (this.direction === "right" && newDirection === "left") {
@@ -168,12 +163,16 @@ Snake.prototype.setDirection = function(newDirection) {
   } else if (this.direction === "left" && newDirection === "right") {
     return;
   }
-  this.nextDirection = newDirection;
+  if (Object.values(directions).includes(newDirection)) {
+    this.nextDirection = newDirection;
+  }
 };
+//an apple constructor
 let Apple = function () {
   this.position = new Block(10, 10);
 };
 
+//apple methods
 Apple.prototype.draw = function () {
   this.position.drawCircle('Red')
 };
@@ -186,12 +185,13 @@ Apple.prototype.move = function () {
     new Block(randomCol, randomRow)
   }
 };
-
+//new instances of snake and apple
 var snake = new Snake();
-var snake2 = new Snake2();
+//var snake2 = new Snake2();
 var apple = new Apple();
 
 
+//the Game 
 let intervalId = setInterval(function () {
   ctx.clearRect(0, 0, width, height);
   drawScore();
@@ -200,38 +200,58 @@ let intervalId = setInterval(function () {
   apple.draw();
   drawBorder();
 }, 100);
+  //let Snake2 = function () {
+    //this.segments = [
+      //new Block(10, 2),
+     // new Block(11, 2),
+      //new Block(12, 3),
+      //new Block(13, 2)
+    //]
+    //this.direction = "right";
+    //this.nextDirection = "right"
+  //}
 
 let pauseGame = function() {
-  clearInterval(intervalId);
-  score;
+  clearInterval(intervalId);  
 }
-  let startTheGame = function () {
-      this.snake.move();
-  }
-
+let startTheGame = function () {
+  isGameOver = false;
+    intervalId = setInterval(function () {
+      ctx.clearRect(0, 0, width, height);
+      drawScore();
+      snake.move();
+      snake.draw();
+      apple.draw();
+      drawBorder();
+    }, 100);
+}
+//an object of keyCodes
 let directions = {
-  13: "enter",//go
-  32: "pause",// space,
   37: "left",
   38: "up",
   39: "right",
   40: "down",
 };
-
+//key strokes 
 $('body').keydown(function(event){
   let newDirection = directions[event.keyCode];
   if (newDirection !== undefined) {
     snake.setDirection(newDirection);
+    
   }
   switch (event.keyCode) {
-    case 32: 
+    case 32:
       pauseGame();
       break;
-    case 18:
-      startTheGame(snake);
+    case 13:
+      if (isGameOver) {
+        snake = new Snake()
+        score = 0
+      }
+      startTheGame();
       break;
-      default:
-        console.log("press the key")
+    default:
+      console.log("press the key")
   }
 })
 });
